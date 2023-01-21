2f60e8a9ff1af51e333ff2bbfdc51884a990a356
rukmini <rukminichalla256@gmail.com>
2023-01-21 16:41:29 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,16 +9,17 @@ public class Main {
         public static class B extends A{
             @Override
             public String meth(){
-                return "Method is overridden in Extended class B";
+                return "Method is overridden in Extended class B ";
             }
-
+            protected Object clone() throws CloneNotSupportedException {
+                return super.clone();
+            }
         }
 
     public static void main(String[] args) {
         B b= new B();
         b.meth();
         B b1= new B();
-        b1.meth();
     }
   
 }
